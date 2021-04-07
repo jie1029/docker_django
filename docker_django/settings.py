18de634736152d73eb87499d78d63f3cb6da3d23
Ji Kyu-Bin <jikyubin72@gmail.com>
2021-04-07 23:15:04 +0900

    장고 프로젝트명 바꿔보기

--- docker_django/settings.py
+++ docker_django/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'docker_django.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'docker_django.wsgi.application'
 
 
 # Database
